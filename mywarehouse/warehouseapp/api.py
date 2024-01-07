7b10c84bda2b6c0e0e46160154e894247f3a92bb
Kamil Kalicki <wspolny.motyw@gmail.com>
2024-01-07 18:30:26 +0100

    show detail view added.

--- mywarehouse/warehouseapp/api.py
+++ mywarehouse/warehouseapp/api.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Product
-from .serializers import ReceiveProductSerializer, IssueProductSerializer
+from .serializers import IssueProductSerializer, ReceiveProductSerializer
+
 
 class ReceiveProductViewApi(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -31,55 +34,31 @@ class ReceiveProductViewApi(generics.ListCreateAPIView):
             return Response({'message': f'Added new product: {product_name.title().strip()} x {quantity}'},
                             status=status.HTTP_201_CREATED)
 
-class IssueProductViewApi(generics.UpdateAPIView):
+class IssueProductViewApi(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = IssueProductSerializer
-    lookup_field = 'name'
-    lookup_url_kwarg = 'name'
 
-    def get(self, request, *args, **kwargs):
-        queryset = Product.objects.all()
-        serializer = IssueProductSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-    def perform_update(self, serializer):
-        product_name = self.kwargs['name']
-        quantity = serializer.validated_data['quantity']
+        product_name = serializer.validated_data['name']
+        quantity_to_issue = serializer.validated_data['quantity']
 
-        try:
-            product = Product.objects.get(name=product_name)
-            if product.quantity >= quantity:
-                if product.quantity > quantity:
-                    product.quantity -= quantity
-                    product.save()
-                else:
-                    product.delete()
-                return Response({'message': f'{quantity} x {product_name} issued.'}, status=status.HTTP_200_OK)
+        product = get_object_or_404(Product, name=product_name)
+
+        if product.quantity >= quantity_to_issue > 0:
+            if product.quantity == quantity_to_issue:
+                product.delete()
             else:
-                return Response({'error': 'Insufficient stock.'}, status=status.HTTP_400_BAD_REQUEST)
-        except Product.DoesNotExist:
-            return Response({'error': f'Product {product_name} not found.'}, status=status.HTTP_404_NOT_FOUND)
+                product.quantity -= quantity_to_issue
+                product.save()
+            return Response({'message': f'{quantity_to_issue} x {product_name} issued.'},
+                            status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid quantity or insufficient stock.'},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ReceiveProductSerializer
-
-    def perform_update(self, serializer):
-        instance = serializer.save()
-        quantity_to_issue = self.request.data.get('quantity')
-
-        # if quantity_to_issue <= 0:
-        #     return Response({'error': 'Quantity should be a positive value.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if instance.quantity >= quantity_to_issue:
-            instance.quantity -= quantity_to_issue
-
-            if instance.quantity == 0:
-                instance.delete()
-                return Response({'message': f'Product {instance.name} fully issued.'}, status=status.HTTP_200_OK)
-            else:
-                instance.save()
-                return Response({'message': f'{quantity_to_issue} x {instance.name} issued.'},
-                                status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Insufficient stock.'}, status=status.HTTP_400_BAD_REQUEST)
--- mywarehouse/warehouseapp/serializers.py
+++ mywarehouse/warehouseapp/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
 from .models import Product
 
-class BaseProductSerializer(serializers.ModelSerializer):
+# class BaseProductSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields = '__all__'
+#
+#     def validate_name(self, value):
+#         return value.title().strip()
+#
+#     def validate(self, data):
+#         if data['price'] <= 0 or data['quantity'] <= 0:
+#             raise serializers.ValidationError("Price and quantity should be positive values.")
+#         return data
+
+#
+class ReceiveProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
@@ -13,14 +27,22 @@ class BaseProductSerializer(serializers.ModelSerializer):
         if data['price'] <= 0 or data['quantity'] <= 0:
             raise serializers.ValidationError("Price and quantity should be positive values.")
         return data
+#
+class IssueProductSerializer(serializers.ModelSerializer):
+    price = serializers.FloatField(read_only=True)
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'quantity']
+
+    def validate_name(self, value):
+        return value.title().strip()
+
+    def validate(self, data):
+        if data['quantity'] <= 0:
+            raise serializers.ValidationError("Quantity should be positive value.")
+        return data
 
 
-class ReceiveProductSerializer(BaseProductSerializer):
-    class Meta(BaseProductSerializer.Meta):
-        pass
 
-class IssueProductSerializer(BaseProductSerializer):
-    price = serializers.FloatField(required=False, default=None, allow_null=True)
-    class Meta(BaseProductSerializer.Meta):
-        fields = ['name', 'quantity', 'price']
+
 
